bb32aed70f1862b02ec1728eba2d63ce04c2d8af
Bhaiya HN Singh <72096831+Bhaiyahns45@users.noreply.github.com>
2022-05-03 20:34:54 +0530

    Update 3may22.py
    2 approach added

--- 3may22.py
+++ 3may22.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+## 1st approach
+
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
 
@@ -22,3 +24,31 @@ class Solution:
                 break
 
         return ed - st + 1
+    
+    
+ ## 2nd approach
+    
+    
+import numpy as np
+
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        
+        
+        if sorted(nums) == nums:
+            return 0
+        
+        a=nums
+        
+        a = np.array((a))
+        sorted_index = np.argsort(a)
+                
+        l , h = 0, len(a)-1
+
+        for i in range(len(a)):
+            if sorted_index[i] != i:
+                l = max(l, sorted_index[i])
+                h = min(h, sorted_index[i])
+                
+                
+        return l-h+1

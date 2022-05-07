4d92dcb5fff15610cc35224da35d98695e7db131
Bhaiya HN Singh <72096831+Bhaiyahns45@users.noreply.github.com>
2022-05-07 23:01:46 +0530

    Update 3may22.py

--- 3may22.py
+++ 3may22.py
@@ -1,21 +1,6 @@
 
 '''
-
-Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in 
-ascending order, then the whole array will be sorted in ascending order.
-
-
-ex-1
-
-Input: nums = [2,6,4,8,10,9,15]
-Output: 5
-Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
-
-ex-2
-
-Input: nums = [1,2,3,4]
-Output: 0
-
+581. Shortest Unsorted Continuous Subarray
 '''
 
 import numpy as np
